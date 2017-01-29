18bdf37786e65162d4a1e3ee6fd3f2524f988110
Maggie <gilbert.539@osu.edu>
2017-01-29 16:07:03 -0500

    got stylesheet attached to layout.vue

--- FrugalApp/views/layout.vue
+++ FrugalApp/views/layout.vue
@@ -2,10 +2,11 @@
     <!DOCTYPE html>
     <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-            <script src="https://unpkg.com/vue/dist/vue.js"></script>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+           <meta charset="UTF-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+           <script src="https://unpkg.com/vue/dist/vue.js"></script>
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+           <link href="../../stylesheets/style.css" rel="stylesheet">
         </head>
         <body>
             {{{app}}}
